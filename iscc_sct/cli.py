da6d50df2d9873b618ac2f22d547bad0c2fb2660
Titusz Pan <titusz.pan@gmail.com>
2024-06-03 23:15:13 +0200

    Add basic cli tool

--- iscc_sct/cli.py
+++ iscc_sct/cli.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
 import argparse
 import glob
 import os
+from loguru import logger
 from iscc_sct.main import create
 
 
@@ -9,14 +9,20 @@ def main():
     parser = argparse.ArgumentParser(description="Generate Semantic Text-Codes for text files.")
     parser.add_argument("path", type=str, help="Path to text files (supports glob patterns).")
     parser.add_argument("--granular", action="store_true", help="Activate granular processing.")
+    parser.add_argument("--debug", action="store_true", help="Show debugging messages.")
     args = parser.parse_args()
+    if not args.debug:
+        logger.remove()
 
     for filepath in glob.glob(args.path):
         if os.path.isfile(filepath):
-            with open(filepath, 'r', encoding='utf-8') as file:
+            with open(filepath, "rt", encoding="utf-8") as file:
                 text = file.read()
                 sct_meta = create(text, granular=args.granular)
-                print(f"File: {filepath}\nSemantic Text-Code: {sct_meta.iscc}\n")
+                if args.granular:
+                    print(sct_meta.model_dump_json(indent=2, exclude_none=True))
+                else:
+                    print(sct_meta.iscc)
 
 
 if __name__ == "__main__":
