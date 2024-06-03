a9ebcc579f18b4391d02e96d46c8e5f401ffd941
Titusz Pan <titusz.pan@gmail.com>
2024-06-03 21:33:20 +0200

    Update SctFeature model to include a 'size' field and remove unnecessary method from SctMeta model.

--- iscc_sct/models.py
+++ iscc_sct/models.py
@@ -8,8 +8,9 @@ __all__ = [
 
 
 class SctFeature(BaseModel):
-    feature: Optional[List[str]] = None
+    feature: Optional[str] = None
     offset: Optional[int] = None
+    size: Optional[int] = None
     text: Optional[str] = None
 
 
@@ -19,22 +20,3 @@ class SctMeta(BaseModel):
     embedding: Optional[List[float]] = None
     features: Optional[List[SctFeature]] = None
 
-    @classmethod
-    def from_meta(cls, data: Dict[str, Any]) -> "SctMeta":
-        # Initialize optional fields to None if they are not present in the data
-        characters = data.get("characters")
-        embedding = data.get("embedding")
-        raw_features = data.get("features", [])
-
-        # Convert features if present
-        features = [
-            SctFeature(feature=[f], offset=idx, text=chunk)
-            for idx, (f, chunk) in enumerate(zip(raw_features, data.get("chunks", [])))
-        ]
-
-        return cls(
-            iscc=data["iscc"],
-            characters=characters,
-            embedding=embedding,
-            features=features if features else None,
-        )
