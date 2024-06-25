9a1635e9fd8f19ade1da80c429bed2d3cac4f199
Titusz Pan <titusz.pan@gmail.com>
2024-06-25 20:02:27 +0200

    Handle text decoding errors

--- iscc_sct/cli.py
+++ iscc_sct/cli.py
@@ -1,8 +1,9 @@
 import argparse
 import glob
-import os
+from pathlib import Path
 from loguru import logger
 from iscc_sct.main import create
+from charset_normalizer import from_bytes
 
 
 def main():
@@ -20,10 +21,25 @@ def main():
     if not args.debug:
         logger.remove()
 
-    for filepath in glob.glob(args.path):
-        if os.path.isfile(filepath):
-            with open(filepath, "rt", encoding="utf-8") as file:
-                text = file.read()
+    for path in glob.glob(args.path):
+        path = Path(path)
+        if path.is_file():
+            logger.debug(f"Processing {path.name}")
+            with path.open("rb") as file:
+                data = file.read()
+                try:
+                    text = data.decode("utf-8")
+                    if not text.strip():
+                        logger.warning(f"SKIPPED empty: {path}")
+                        continue
+                except UnicodeDecodeError:
+                    logger.debug(f"Could not decode {path.name} as UTF-8.")
+                    charset_match = from_bytes(data).best()
+                    if not charset_match:  # pragma: no cover
+                        logger.error(f"SKIPPING {path.name} - failed to detect text encoding")
+                        continue
+                    logger.debug(f"Decode {path.name} with {charset_match.encoding}.")
+                    text = str(charset_match)
                 sct_meta = create(text, granular=args.granular, bits=args.bits)
                 if args.granular:
                     print(sct_meta.model_dump_json(indent=2, exclude_none=True))
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,10 +12,37 @@ def sample_text_file(tmp_path):
     return file_path
 
 
+@pytest.fixture
+def empty_text_file(tmp_path):
+    file_path = tmp_path / "empty.txt"
+    file_path.write_text(" ")
+    return file_path
+
+
+@pytest.fixture
+def non_utf8_text_file(tmp_path):
+    file_path = tmp_path / "non_utf8.txt"
+    file_path.write_text("Iñtërnâtiônàlizætiøn☃", encoding="utf-16")
+    return file_path
+
+
 def test_cli_no_args():
     result = subprocess.run([sct], capture_output=True, text=True)
     assert result.returncode == 0
     assert "Generate Semantic" in result.stdout
+
+
+def test_cli_empty_file(empty_text_file):
+    result = subprocess.run([sct, str(empty_text_file), "-d"], capture_output=True, text=True)
+    assert result.returncode == 0
+    assert "SKIPPED" in result.stderr
+
+
+def test_cli_non_utf8_file(non_utf8_text_file):
+    result = subprocess.run([sct, str(non_utf8_text_file), "-d"], capture_output=True, text=True)
+    assert result.returncode == 0
+    assert "Could not decode" in result.stderr
+    assert "ISCC:" in result.stdout
 
 
 def test_cli_generate_sct(sample_text_file):
