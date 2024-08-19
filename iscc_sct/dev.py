0060c05ef65349558e06b748125be0c3c19f0fd2
Titusz Pan <titusz.pan@gmail.com>
2024-08-19 08:22:01 +0200

    Add yaml formating

--- iscc_sct/dev.py
+++ iscc_sct/dev.py
@@ -1,4 +1,5 @@
 import pathlib
+import yaml
 
 
 HERE = pathlib.Path(__file__).parent.absolute()
@@ -20,3 +21,22 @@ def convert_lf():  # pragma: no cover
                     outfile.write(content)
                 n += 1
     print(f"{n} files converted to LF")
+
+
+def format_yml():
+    for f in HERE.glob("**\*.yml"):
+        with open(f, "rt", encoding="utf-8") as infile:
+            data = yaml.safe_load(infile)
+        with open(f, "wt", encoding="utf-8", newline="\n") as outf:
+            yaml.safe_dump(
+                data,
+                outf,
+                indent=2,
+                width=80,
+                encoding="utf-8",
+                sort_keys=False,
+                default_flow_style=False,
+                default_style=">",
+                allow_unicode=True,
+                line_break="\n"
+            )
